21e3d89bd8997396790d86174c31e1b0eaec54cf
Sanaullah <sanaullahsolangi817@gmail.com>
2024-02-05 15:49:04 +0500

    Initial commit

---
+++ assets/app.js
@@ -0,0 +1,20 @@
+var students = ["sani", "sami", "paro", "farooque", "zain"];
+
+// console.log("checing", students.length);
+
+// var newData = students.slice(0,3)
+// console.log("ab men students.slice(0,3) krdya hai",newData)
+// console.log("jab slice kya uske baad jo original tha",students)
+
+// var newData2 = students.splice(0,2, "sanaullah")
+// console.log("jab splice kya or uske variable ko print kya to aya",newData2)
+
+// console.log("jo original tha splice k baad bhi hai",students)
+
+// var table = 2;
+// for (var i = 1; i <= 10; i++) {
+//   console.log(`${table} x ${i} = ${table * i}`);
+// }
+
+var greet = (a, b) => a + b;
+console.log(greet(25, 25));
