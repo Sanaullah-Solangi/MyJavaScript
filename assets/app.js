abbd53c48a89411c873693d4a749178bb6efd53e
Sanaullah <sanaullahsolangi817@gmail.com>
2024-02-07 16:56:15 +0500

    subMenu's functionality and styling is still continued and is incomplete, soon it will be completed inshallah.

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,14 @@
+//!=== getting elements ===
+
 let header = document.querySelector(".header");
 let anchors = document.querySelectorAll(".navbar a");
 let toggleBtns = document.querySelector(".toggle-btns");
 let linkCover = document.querySelector(".link-cover");
 let toggleMenu = document.querySelector(".toggle-menu");
 let navbar = document.querySelector(".navbar");
+let subMenuBtns = document.querySelectorAll(".subMenu-toggle-btns");
+
+//!=== header functionality ===
 
 toggleBtns.addEventListener("click", () => {
   header.classList.toggle("show");
@@ -15,28 +20,16 @@ anchors.forEach((val) => {
   });
 });
 
+//!=== subMenu & mob-navbar functionality ===
+
 toggleMenu.addEventListener("click", () => {
   linkCover.classList.toggle("showMenu");
   navbar.classList.toggle("showMenu");
 });
 
-var students = ["sani", "sami", "paro", "farooque", "zain"];
-
-// console.log("checing", students.length);
-
-// var newData = students.slice(0,3)
-// console.log("ab men students.slice(0,3) krdya hai",newData)
-// console.log("jab slice kya uske baad jo original tha",students)
-
-// var newData2 = students.splice(0,2, "sanaullah")
-// console.log("jab splice kya or uske variable ko print kya to aya",newData2)
-
-// console.log("jo original tha splice k baad bhi hai",students)
-
-// var table = 2;
-// for (var i = 1; i <= 10; i++) {
-//   console.log(`${table} x ${i} = ${table * i}`);
-// }
-
-var greet = (a, b) => a + b;
-console.log(greet(25, 25));
+subMenuBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    let subMenuCover = btn.parentElement;
+    subMenuCover.classList.toggle("showSubMenu");
+  });
+});
