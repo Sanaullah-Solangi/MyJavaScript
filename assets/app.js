ba1453dd5634e97e8da19ace687823dff16811cb
Sanaullah <sanaullahsolangi817@gmail.com>
2024-02-14 18:13:05 +0500

    many improvements are done in navbar

--- assets/app.js
+++ assets/app.js
@@ -1,13 +1,15 @@
 //!=== getting elements ===
 
 let header = document.querySelector(".header");
-let anchors = document.querySelectorAll(".navbar a");
+let anchors = document.querySelectorAll(".navbar > a");
 let toggleBtns = document.querySelector(".toggle-btns");
 let linkCover = document.querySelector(".link-cover");
 let toggleMenu = document.querySelector(".toggle-menu");
 let navbar = document.querySelector(".navbar");
 let subMenuBtns = document.querySelectorAll(".subMenu-toggle-btns");
+let subMenu;
 let subMenuWrapper = document.querySelector(".subMenu-cover");
+let subMenuAnchors = document.querySelectorAll(".subMenu-item > a");
 
 //!=== header functionality ===
 
@@ -18,6 +20,16 @@ toggleBtns.addEventListener("click", () => {
 anchors.forEach((val) => {
   val.addEventListener("click", () => {
     header.classList.remove("show");
+  });
+});
+
+//!=== removing navbar and subMenu with on click
+
+subMenuAnchors.forEach((anchor) => {
+  anchor.addEventListener("click", () => {
+    header.classList.remove("show");
+    let subMenuAnchorParent = anchor.parentElement.parentElement;
+    subMenuAnchorParent.classList.remove("showSubMenu");
   });
 });
 
